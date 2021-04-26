5c47401cb8cc0020105d34f017dc5c9bf01ce3c0
Dylan DesRosier <dylan@pooltogether.com>
2021-04-26 19:30:36 -0400

    remove dist and node modules

--- dist/_utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bn = void 0;
-const ethers_1 = require("ethers");
-exports.bn = ethers_1.ethers.BigNumber.from;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiX3V0aWxzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL191dGlscy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxtQ0FBK0I7QUFFbEIsUUFBQSxFQUFFLEdBQUcsZUFBTSxDQUFDLFNBQVMsQ0FBQyxJQUFJLENBQUEifQ==
