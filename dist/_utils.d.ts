a1f79345ca0872bdb47e4303f07017d09ee723b3
Dylan DesRosier <dylan@pooltogether.com>
2021-04-27 11:48:06 -0400

    remove dist, instead ignore in npmignore

--- dist/_utils.d.ts
+++
@@ -1,2 +0,0 @@
-import { ethers } from 'ethers';
-export declare const bn: typeof ethers.BigNumber.from;
--- dist/keys.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const getLootBoxKey: (lootBoxAddress: string, lootBoxId: string) => string;
