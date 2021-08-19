02258844ee95e0e787e3649617afef9e6d5cd96f
Charles Bergeron <chuck.bergeron@gmail.com>
2021-08-19 16:25:09 -0700

    * start bringing over graphql queries

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,16 @@ import * as FormatData from './formatData'
 import * as FormatNumber from './formatNumber'
 import * as Keys from './keys'
 import * as Network from './networks'
+import * as PoolData from './poolDataUtils'
 import * as PTMath from './math'
 import * as QueryParams from './queryParams'
+import * as Subgraph from './subgraph'
 import * as Time from './time'
 import * as Tokens from './tokens'
 import * as YieldSources from './yieldSources'
+
+// Queries
+import * as AccountQuery from './queries/accountQuery'
 
 // Data
 import * as NetworkData from './data/networks'
@@ -22,11 +27,15 @@ export = {
   ...FormatNumber,
   ...Keys,
   ...Network,
+  ...PoolData,
   ...PTMath,
   ...QueryParams,
+  ...Subgraph,
   ...Time,
   ...Tokens,
   ...YieldSources,
+  // Queries
+  ...AccountQuery,
   // Data
   ...NetworkData,
   ...YieldSourcesData
