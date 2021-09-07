5d19e4b4aca8716d027f3c6710bb2ab436920b9d
Dylan DesRosier <dylan@pooltogether.com>
2021-09-07 16:44:59 -0400

    close as we can get calculating shares and underlying tokens for pods

--- src/__tests__/pods.test.ts
+++ src/__tests__/pods.test.ts
@@ -1,30 +1,59 @@
-import { parseUnits } from '@ethersproject/units'
+import { BigNumber } from '@ethersproject/bignumber'
+import { formatUnits, parseUnits } from '@ethersproject/units'
 import { underlyingAmountToSharesAmount, sharesAmountToUnderlyingAmount } from '../pods'
 
 describe('underlyingAmountToSharesAmount', () => {
   it('1:1', () => {
     const decimals = '2'
     const value = parseUnits('1', decimals)
-    expect(underlyingAmountToSharesAmount(value, value, decimals)).toEqual(value)
+    expect(underlyingAmountToSharesAmount(value, value, value)).toEqual(value)
   })
 
   it('Basic conversion', () => {
     const decimals = '2'
     const underlyingAmount = parseUnits('10', decimals)
-    const conversionRate = parseUnits('1.5', decimals)
+    const sharesTotalSupply = parseUnits('6.66', decimals)
+    const podUnderlyingTokenTotalBalance = parseUnits('10', decimals)
     const expectedAmount = parseUnits('6.66', decimals)
 
     console.log(
       `Result: ${underlyingAmountToSharesAmount(
         underlyingAmount,
-        conversionRate,
-        decimals
+        sharesTotalSupply,
+        podUnderlyingTokenTotalBalance
       ).toString()}, expected: ${expectedAmount.toString()}`
     )
 
-    expect(underlyingAmountToSharesAmount(underlyingAmount, conversionRate, decimals)).toEqual(
-      expectedAmount
+    expect(
+      underlyingAmountToSharesAmount(
+        underlyingAmount,
+        sharesTotalSupply,
+        podUnderlyingTokenTotalBalance
+      )
+    ).toEqual(expectedAmount)
+  })
+
+  it('Real example', () => {
+    const decimals = '18'
+    const underlyingAmount = BigNumber.from('10000000000000000000001')
+    const sharesTotalSupply = BigNumber.from('21233595596753931222690')
+    const podUnderlyingTokenTotalBalance = BigNumber.from('11261083921989257612879').add(
+      BigNumber.from('10000000000000000000004')
     )
+    const expectedAmountUnformatted = BigNumber.from('9987071061223319562909')
+    const expectedAmountString = formatUnits(expectedAmountUnformatted, decimals)
+    const expectedAmountNumber = Number(expectedAmountString)
+
+    const sharesAmountUnformatted = underlyingAmountToSharesAmount(
+      underlyingAmount,
+      sharesTotalSupply,
+      podUnderlyingTokenTotalBalance
+    )
+    const sharesAmountString = formatUnits(sharesAmountUnformatted, decimals)
+    const sharesAmountNumber = Number(sharesAmountString)
+    // There's some rounding uglyness.
+    // We can end up off by 0.000000000000000001
+    expect(sharesAmountNumber).toEqual(expectedAmountNumber)
   })
 })
 
@@ -32,25 +61,39 @@ describe('sharesAmountToUnderlyingAmount', () => {
   it('1:1', () => {
     const decimals = '2'
     const value = parseUnits('1', decimals)
-    expect(sharesAmountToUnderlyingAmount(value, value, decimals)).toEqual(value)
+    expect(sharesAmountToUnderlyingAmount(value, value, value)).toEqual(value)
   })
 
   it('Basic conversion', () => {
     const decimals = '2'
     const sharesAmount = parseUnits('10', decimals)
-    const conversionRate = parseUnits('1.5', decimals)
+    const sharesTotalSupply = parseUnits('10', decimals)
+    const podUnderlyingTokenTotalBalance = parseUnits('15', decimals)
     const expectedAmount = parseUnits('15', decimals)
 
-    console.log(
-      `Result: ${sharesAmountToUnderlyingAmount(
+    expect(
+      sharesAmountToUnderlyingAmount(
         sharesAmount,
-        conversionRate,
-        decimals
-      ).toString()}, expected: ${expectedAmount.toString()}`
-    )
+        sharesTotalSupply,
+        podUnderlyingTokenTotalBalance
+      )
+    ).toEqual(expectedAmount)
+  })
 
-    expect(sharesAmountToUnderlyingAmount(sharesAmount, conversionRate, decimals)).toEqual(
-      expectedAmount
+  it('Real example', () => {
+    const sharesAmount = BigNumber.from('9987071061223319562909')
+    const sharesTotalSupply = BigNumber.from('21233595596753931222690')
+    const podUnderlyingTokenTotalBalance = BigNumber.from('11261083921989257612879').add(
+      BigNumber.from('10000000000000000000004')
     )
+    const expectedAmount = BigNumber.from('10000000000000000000001')
+
+    expect(
+      sharesAmountToUnderlyingAmount(
+        sharesAmount,
+        sharesTotalSupply,
+        podUnderlyingTokenTotalBalance
+      )
+    ).toEqual(expectedAmount)
   })
 })
--- src/pods.ts
+++ src/pods.ts
@@ -2,31 +2,37 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { parseUnits } from '@ethersproject/units'
 
 /**
- * All values are unformatted (contain decimal padding)
- * @param underlyingAmount the amount to convert to shares
- * @param pricePerShare the conversion rate
- * @param decimals the decimals used for the pod
- * @returns share amount
+ *
+ * @param underlyingAmount
+ * @param sharesTotalSupply
+ * @param podUnderlyingTokenTotalBalance
+ * @returns
  */
 export const underlyingAmountToSharesAmount = (
   underlyingAmount: BigNumber,
-  pricePerShare: BigNumber,
-  decimals: string = '18'
+  sharesTotalSupply: BigNumber,
+  podUnderlyingTokenTotalBalance: BigNumber
 ): BigNumber => {
-  return underlyingAmount.mul(parseUnits('1', decimals)).div(pricePerShare)
+  if (podUnderlyingTokenTotalBalance.isZero()) {
+    return undefined
+  }
+  return underlyingAmount.mul(sharesTotalSupply).div(podUnderlyingTokenTotalBalance)
 }
 
 /**
- * All values are unformatted (contain decimal padding)
- * @param sharesAmount the amount to convert to underlying tokens
- * @param pricePerShare the conversion rate
- * @param decimals the decimals used for the pod
- * @returns underlying amount
+ *
+ * @param sharesAmount
+ * @param sharesTotalSupply
+ * @param podUnderlyingTokenTotalBalance
+ * @returns
  */
 export const sharesAmountToUnderlyingAmount = (
   sharesAmount: BigNumber,
-  pricePerShare: BigNumber,
-  decimals: string = '18'
+  sharesTotalSupply: BigNumber,
+  podUnderlyingTokenTotalBalance: BigNumber
 ): BigNumber => {
-  return sharesAmount.mul(pricePerShare).div(parseUnits('1', decimals))
+  if (sharesTotalSupply.isZero()) {
+    return undefined
+  }
+  return sharesAmount.mul(podUnderlyingTokenTotalBalance).div(sharesTotalSupply)
 }
