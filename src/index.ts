16c1021e4d50ac354380e6c0c1b28f72a11466b2
Dylan DesRosier <dylan@pooltogether.com>
2021-11-29 11:07:11 -0500

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,15 @@
 // Functions
-import * as Address from './address'
-import * as FormatData from './formatData'
-import * as FormatNumber from './formatNumber'
-import * as Keys from './keys'
-import * as Network from './networks'
-import * as Pods from './pods'
-import * as Providers from './providers'
-import * as PTMath from './math'
-import * as Time from './time'
-import * as Tokens from './tokens'
-import * as Odds from './odds'
+export * from './address'
+export * from './formatData'
+export * from './formatNumber'
+export * from './keys'
+export * from './networks'
+export * from './pods'
+export * from './providers'
+export * from './math'
+export * from './time'
+export * from './tokens'
+export * from './odds'
 
 // Data
-import * as NetworkData from './data/networks'
-
-// Export for use as a npm package
-const exports = {
-  // Functions
-  ...Address,
-  ...Odds,
-  ...FormatData,
-  ...FormatNumber,
-  ...Keys,
-  ...Network,
-  ...Pods,
-  ...Providers,
-  ...PTMath,
-  ...Time,
-  ...Tokens,
-  // Data
-  ...NetworkData
-}
-
-export default exports
+export * from './data/networks'
