8b02eced505f8fc2f6d3ea3a615c4bc74a097fa5
Pierrick Turelier <pierrick@turelier.com>
2022-08-22 16:42:56 -0500

    feat(networks): add Goerli support

--- src/data/networks.ts
+++ src/data/networks.ts
@@ -15,13 +15,15 @@ export const NETWORK = Object.freeze({
   'xdai': 100,
   'polygon': 137,
   'matic': 137,
+  'mumbai': 80001,
   'optimism': 10,
-  'optimism-kovan': 69,
+  'optimism-goerli': 420,
   'avalanche': 43114,
   'fuji': 43113,
   'celo': 42220,
   'celo-testnet': 44787,
-  'mumbai': 80001
+  'arbitrum': 42161,
+  'arbitrum-goerli': 421613,
 })
 
 /**
@@ -29,12 +31,10 @@ export const NETWORK = Object.freeze({
  */
 export const ETHEREUM_NETWORKS = Object.freeze([
   NETWORK.mainnet,
-  NETWORK.ropsten,
-  NETWORK.rinkeby,
   NETWORK.goerli,
-  NETWORK.kovan
 ])
 export const POLYGON_NETWORKS = Object.freeze([NETWORK.polygon, NETWORK.mumbai])
 export const AVALANCHE_NETWORKS = Object.freeze([NETWORK.avalanche, NETWORK.fuji])
 export const CELO_NETWORKS = Object.freeze([NETWORK.celo, NETWORK['celo-testnet']])
-export const OPTIMISM_NETWORKS = Object.freeze([NETWORK.optimism, NETWORK['optimism-kovan']])
+export const OPTIMISM_NETWORKS = Object.freeze([NETWORK.optimism, NETWORK['optimism-goerli']])
+export const ARBITRUM_NETWORKS = Object.freeze([NETWORK.arbitrum, NETWORK['arbitrum-goerli']])
